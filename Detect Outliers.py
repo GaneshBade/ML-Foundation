6b2d689bb22d11203d4c926c82359cb0dec68c3f
Ganesh Bade <55490435+GaneshBade@users.noreply.github.com>
2021-08-04 23:38:32 +0530

    Detect Outliers
    Detect Outliers

--- Detect Outliers.py
+++ Detect Outliers.py
@@ -22,5 +22,50 @@ def detect_outlier(data):
         if np.abs(z_score) > threshold:
             print(f"Outlier data: {i}")
             outlier.append(i)
+    return outlier
 
 outlier_pts = detect_outlier(dataset)
+
+#  Detect outlier using IQR
+#  Steps:
+    # 1. sort data in ascending order
+    # 2. calculate q1 and q3
+    # 3. Find interquartile range
+    # 4. Find lower boundary 1.5*q1
+    # 5. Find upper bound 1.5*q3
+# Any data point that lies outside of lower and upper bound will be considered as outlier
+outlier_iqr = []
+def detect_outlier_IQR(data):
+    sort_data = sorted(data)
+    q1, q3 = np.percentile(sort_data, [25, 75])
+
+    iqr = q3 - q1
+
+    iqr_lower_bound = q1 - (1.5 * iqr)
+    iqr_upper_bound = q3 + (1.5 * iqr)
+
+    # print(f"Data : {data}", end=" ")
+    # print(f"iqr_lower_bound: {iqr_lower_bound}")
+    # print(f"iqr_upper_bound: {iqr_upper_bound}")
+
+    for i in data:
+        if (i < iqr_lower_bound) | (i > iqr_upper_bound):
+            outlier_iqr.append(i)
+    return outlier_iqr
+
+outlierByIQR = detect_outlier_IQR(dataset)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
