5fc501b41cb5bc1c2dc89ddbbeb41a220d8486f3
Ganesh Bade <55490435+GaneshBade@users.noreply.github.com>
2021-08-04 23:40:59 +0530

    Detect Outliers
    Detect Outliers

--- Detect Outliers.py
+++ Detect Outliers.py
@@ -4,10 +4,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Data points that falls outside of 1.5 times IQR
-
 dataset= [11,10,12,14,12,15,14,13,15,102,12,14,17,19,107, 10,13,12,14,12,108,12,11,14,13,15,10,15,12,10,14,13,15,10]
 
+#  Detect outliers using z-score
 outlier = []
 def detect_outlier(data):
 
